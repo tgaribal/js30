0925fc52a6fc37a157b85bc3a18dbfb0cc10a352
Tim Garibaldi <tgaribal@gmail.com>
2017-04-04 23:12:54 -0700

    Setup basic router

--- github-battle/app/Components/Home.js
+++ github-battle/app/Components/Home.js
@@ -1,1 +1,11 @@
+var React = require('react');
 
+var Home = React.createClass({
+  render: function () {
+    return (
+      <div> Hello from Home! </div>
+    )
+  }
+});
+
+module.exports = Home;
--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,63 +1,67 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var USER = {
-  name: 'Tim Garibaldi',
-  username: 'tgaribal',
-  image: 'https://cmgajcradiotvtalk.files.wordpress.com/2017/03/nina-bonina-brown.jpg?w=485&h=485'
-}
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
+ReactDOM.render(routes, document.getElementById('app'));
+// var USER = {
+//   name: 'Tim Garibaldi',
+//   username: 'tgaribal',
+//   image: 'https://cmgajcradiotvtalk.files.wordpress.com/2017/03/nina-bonina-brown.jpg?w=485&h=485'
+// }
+// var ProfilePic = React.createClass({
+//   render: function() {
+//     return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+//   }
+// });
 
-var Link = React.createClass({
-  changeURL: function() {
-    window.location.replace(this.props.href)
-  },
-  render: function () {
-    return (
-      <span style ={{color: 'blue', cursor: 'pointer'}}
-      onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    )
-  }
-})
+// var Link = React.createClass({
+//   changeURL: function() {
+//     window.location.replace(this.props.href)
+//   },
+//   render: function () {
+//     return (
+//       <span style ={{color: 'blue', cursor: 'pointer'}}
+//       onClick={this.changeURL}>
+//         {this.props.children}
+//       </span>
+//     )
+//   }
+// })
 
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Link href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-});
+// var ProfileLink = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <Link href={'https://www.github.com/' + this.props.username}>
+//           {this.props.username}
+//         </Link>
+//       </div>
+//     )
+//   }
+// });
 
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
+// var ProfileName = React.createClass({
+//   render: function() {
+//     return (
+//       <div>{this.props.name}</div>
+//     )
+//   }
+// });
 
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
+// var Avatar = React.createClass({
+//   render: function () {
+//     return (
+//       <div>
+//         <ProfilePic imageUrl={this.props.user.image} />
+//         <ProfileName name={this.props.user.name} />
+//         <ProfileLink username={this.props.user.username} />
+//       </div>
+//     )
+//   }
+// });
 
 
 
-ReactDOM.render(<Avatar user = {USER} />, document.getElementById('app'));
+
+
+
